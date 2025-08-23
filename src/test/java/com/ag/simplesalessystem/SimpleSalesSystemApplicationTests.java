458a0577e9b8204d23dfcf16ae11024633a4586f
AhmadGh <ahmad2000gh@gmail.com>
2025-08-23 21:35:21 +0300

    initialized project

---
+++ src/test/java/com/ag/simplesalessystem/SimpleSalesSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ag.simplesalessystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleSalesSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
